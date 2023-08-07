80e1de2aacb829aaf92cebaa4834e5445c23b757
Daniel Stahl <danstahl1138@gmail.com>
2023-08-07 09:14:43 -0500

    changed entrypoing name

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ import setuptools
 
 setuptools.setup(
     name="jupyter-prefect-proxy",
-    version="1.1dev",
+    version="1.2dev",
     url="https://github.com/danielhstahl/jupyter-prefect-proxy",
     author="Daniel Stahl",
     description="danstahl1138@gmail.com",
     packages=setuptools.find_packages(),
-    keywords=["Jupyter"],
+    keywords=["Jupyter", "Prefect"],
     classifiers=["Framework :: Jupyter"],
     install_requires=["jupyter-server-proxy", "prefect"],
     entry_points={
         "jupyter_serverproxy_servers": [
-            "jupyter-prefect-proxy = jupyter_prefect_proxy:setup_prefect",
+            "prefect = jupyter_prefect_proxy:setup_prefect",
         ]
     },
     package_data={
